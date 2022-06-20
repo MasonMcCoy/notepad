adb9b1f7f36c979d0e3a14ddf9bf38cdabc62c7c
Mason McCoy <mmasonmccoy@gmail.com>
2022-06-20 17:30:22 -0500

    get request now render notes.html and existing notes from db.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const savedNotes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,6 +8,21 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// Renders notes HTML
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+// Returns existing notes to lefthand pane
+app.get('/api/notes', (req, res) => {
+    res.json(savedNotes);
+});
+
+// Add new notes
+app.post('/api/notes', (req, res) => {
+    
+})
 
 
 app.listen(PORT, () =>
