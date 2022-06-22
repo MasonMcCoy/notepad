8895f68f3c78a7fb4895bbf662866e1248d71a26
Mason McCoy <mmasonmccoy@gmail.com>
2022-06-21 22:33:48 -0500

    Added landing route for Heroku deployment?

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // Renders notes HTML
